586444a842a611203fa9b3d72fe80658c7bae9fe
Kevin Wu <wu.haoran2002@gmail.com>
2025-04-26 16:31:47 -0400

    Added elements for login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,25 @@
 function Login() {
   return (
     <div>
-      <h1>ChorePal Login Page</h1>
+      <h1>Welcome to ChorePal</h1>
+      <div>
+        <h2>Login</h2>
+        <form action='/login' method='POST'>
+          <label htmlFor='email'>Email:</label>
+          <br />
+          <input type='text' id='email' name='email' />
+          <br />
+          <br />
+
+          <label htmlFor='password'>Password:</label>
+          <br />
+          <input type='password' id='password' name='password' />
+          <br />
+          <br />
+
+          <button type='submit'>Login</button>
+        </form>
+      </div>
     </div>
   );
 }
