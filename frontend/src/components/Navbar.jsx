bd0d58ff57df12761a46cc061bff1a06b39c77fd
MichalMarrow <marrowmichal@gmail.com>
2025-05-08 21:20:36 -0400

    The following changes include CSS styling for the new Wanted poster and new code on Dashboard and Navbar for the poster logic and location on the web app.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import { Link } from 'react-router-dom';
+import './WantedPoster.css';
 
-const Navbar = () => {
+const Navbar = ({ wantedUser }) => {
   return (
-    <div className='flex items-center'>
-      <img src='/chorepal-logo-optimized.png' width='150' height='150'></img>
+    <div className='flex items-center relative'>
+      <img
+        src='/chorepal-logo-optimized.png'
+        width='150'
+        height='150'
+        alt='ChorePal Logo'
+      />
       <div>
         <h1 className='text-6xl text-black font-extrabold drop-shadow-sm'>
           ChorePal
@@ -11,7 +17,15 @@ const Navbar = () => {
         <h3 className='text-2xl font-semibold text-orange-800 mt-2'>
           Plan it. Do it.
         </h3>
+        <div className='poster-container'>
+          <div className='poster-title'>🔥 Wanted 🔥</div>
+          <div className='poster-user'>
+            {wantedUser ? wantedUser : 'No one... yet'}
+          </div>
+          <div className='poster-divider'>Dead || Alive</div>
+        </div>
       </div>
+
       <div className='ml-auto mr-5'>
         <Link to='/'>
           <button>Log Out</button>
